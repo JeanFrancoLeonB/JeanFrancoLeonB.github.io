cdb448ab55e813ad69848c324f45ad786bc62c88
JeanFrancoLeonB <jeanfra08@hotmail.com>
2020-10-15 06:03:37 -0500

    Preston

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,5 +1,4 @@
-function toggleMenu() {
-    
+function toggleMenu(){
+    consolegetElementById("primaryNav").classList.toggle("hide");
     document.getElementById("primaryNav").classList.toggle("hide");
-}
-
+  }
